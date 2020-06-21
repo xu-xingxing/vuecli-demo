3df7c64aa0ba43de3f3f9ea66951f452e0a95e47
xuxingxing <973553289@qq.com>
2020-06-21 11:45:57 +0800

    vue declaration cycle

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -25,6 +25,31 @@ export default {
       // 可以使用注册事件的方法
        this.$emit('ChageAppTitle', "hello 米斯特okxu通过$emit注册事件修改了title值")
     }
+  },
+  //　生命周期函数
+  beforeCreate() {
+    alert('这个时候是实例还没有被创建，所以你无法知道data,也不能用watch监听')
+  },
+  created() {
+    alert('这个时候实例已经被创建，可以得到data，调用watch监听，但是页面还是为空白的')
+  },
+  beforeMount() {
+    alert('页面挂载前，此时页面依然是空白的，这时render函数被首次调用')
+  },
+  created() {
+    alert('页面已经挂载了，这时你可以看到页面的内容，也可以访问到dom')
+  },
+  beforeUpdate() {
+    alert('数据更新前，也就是虚拟DOM打补丁之前')
+  },
+  updated() {
+    alert('数据已经更新完毕')
+  },
+  beforeDestroy() {
+    alert('页面离开之前被调用，清除定时器，或者三方的一些DOM结构')
+  },
+  destroyed() {
+    alert('实例已经完全被销毁')
   }
 }
 </script>
