d4c6209f33f51ca32b5075e1085ab16b7205afe2
xuxingxing <973553289@qq.com>
2020-06-21 08:32:39 +0800

    vue css and vue scoped css

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <div id="app">
+    <h1>App.vue</h1>
     <!-- 3.调用组件 -->
     <Users/>
     <!-- <my-users></my-users> -->
+    <p>子组件如果想使用父组件的样式，可以将scoped去掉</p>
   </div>
 </template>
 
@@ -20,5 +22,8 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+h1 {
+  color: purple;
+}
 </style>
--- src/components/Users.vue
+++ src/components/Users.vue
@@ -1,8 +1,13 @@
 <template>
   <div id="users">
+    <h1>Users.vue</h1>
     <ul>
       <li v-for="(user, index) in users" :key="index">{{user}}</li>
     </ul>
+    <p class="desc">
+      在style标签中加入scoped之后，当前样式只针对当前组件起作用
+      原理是给当前标签加上了一个唯一的属性值
+    </p>
   </div>
 </template>
 
@@ -16,6 +21,11 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+  h1 {
+    color: green;
+  }
+  .desc {
+    color: red;
+  }
 </style>
