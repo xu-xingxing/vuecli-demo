6d186c7919bdf50e68a7df0c7edec15c0134641f
xuxingxing <973553289@qq.com>
2020-06-21 12:27:40 +0800

    vue slot

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,47 @@
 <template>
   <div id="app">
-    <!-- <Header/> -->
-    <!-- 传值 -->
-    <Header :title="title" @ChageAppTitle="titleChange"></Header>
-    <!-- 3.调用组件 -->
-    <!-- 属性传值： 值有两种情况：1.传值（string, boolean, number) 2.传引用(object, array) -->
-    <Users :users="users"/>
-    <hr>
-    <Users :users="users"/>
-    <!-- <my-users></my-users> -->
-    <!-- <p>子组件如果想使用父组件的样式，可以将scoped去掉</p> -->
-    <Footer :title="title"/>
+    <!-- 方法一使用字符串形式将标签传给子组件 -->
+    <!-- <FormHelper :title="title"/> -->
+    <!-- 方法二使用slot插槽 -->
+    <!-- 此种写法并不能将标签传给子组件 -->
+    <!-- <FormHelper>
+      <h2>这是个title</h2>
+      <p>这个是文本内容</p>
+    </FormHelper> -->
+    <!-- <FormHelper>
+      <h2>这是个title</h2>
+      <p>这个是文本内容</p>
+    </FormHelper> -->
+
+    <!-- 给标签定义一个slot属性指定slot占的位置 -->
+    <FormHelper>
+      <h2 slot="h2">{{ title }}</h2>
+      <p slot="p">给标签定义一个slot属性指定slot占的位置</p>
+    </FormHelper>
   </div>
 </template>
 
 <script>
-// 1.引入组件
-import Users from './components/Users'
-import Header from './components/Header'
-import Footer from './components/Footer'
+import FormHelper from './components/FormHelper'
 export default {
   name: 'App',
   data() {
     return {
-      users: [
-        {name:   "小猪佩奇",age: 10, hobby: '吃饭',show: false},
-        {name: "大象艾米莉",age: 10, hobby: '',show: false},
-        {name:   "小羊苏西",age: 10, hobby: '吃饭睡觉打六六',show: false},
-        {name:   "小猪佩奇",age: 10, hobby: '',show: false},
-        {name: "大象艾米莉",age: 10, hobby: '睡觉打六六',show: false},
-        {name:   "小羊苏西",age: 10, hobby: '',show: false},
-        {name:   "小猪佩奇",age: 10, hobby: '吃饭',show: false}
-      ],
-      title: 'Hello mrokxu'
+      // 起因：如果想给子组件传递标签怎么做？
+      // 1.使用字符串形式
+      title: "这是个title"
     }
   },
   components: {
-    // 2.注册组件
-    Users,
-    // Users:Users
-    // "my-users": Users
-    Header,
-    Footer
+    FormHelper
   },
   methods: {
-    titleChange(val) {
-      console.log(val)
-      this.title = val
-    }
   }
 }
 </script>
 
 <style scoped>
-h1 {
-  color: purple;
-}
+  h2 {
+    background: lightseagreen;
+  }
 </style>
---
+++ src/components/FormHelper.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="form-helper">
+    <h1>vue slot</h1>
+    <!-- 方法一使用字符串形式将标签传给子组件 -->
+    <!-- 将父组件传过来的标签显示出来 -->
+    <!-- <div v-html="title"></div> -->
+
+    <!-- 方法二使用slot接收传给子组件的标签 -->
+    <!-- <p>slot相当于一个占位符，不管传递多少标签都可以显示出来</p>
+    <p>以下是slot插入的内容</p>
+    <slot></slot> -->
+
+    <!-- 方法二使用情况指定占位的地方 -->
+    <slot name="h2"></slot>
+    <p>slot相当于一个占位符，不管传递多少标签都可以显示出来</p>
+    <p>以下是slot插入的内容</p>
+    <slot name="p"></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'form-helper',
+    props: {
+      title: {
+        type: String
+      }
+    },
+    data() {
+      return {
+        
+      }
+    }
+    
+  }
+</script>
+
+<style scoped>
+  /* // 对于给插槽中的标签设定样式，在插入的地方设定或者在使用的顶设定都可以 */
+  h2 {
+    background: lightseagreen;
+  }
+  
+</style>
